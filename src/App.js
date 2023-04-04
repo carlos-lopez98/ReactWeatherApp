ca6a250b93ec13dc0ea6d0711863bb6603e4e953
Carlos <cjimenez9811@gmail.com>
2023-04-04 10:46:32 -0700

    Adding Display Components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,37 @@ import axios from 'axios'
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=545b76c069c48da11f97fe003505ac8a'
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+
+        <div className="top">
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>60 F</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+
+
+        <div className="bottom">
+          <div className='feels'>
+           <p> 65 F </p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
