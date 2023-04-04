f25be6d837222129f80fceaffc14217349ab377b
Carlos <cjimenez9811@gmail.com>
2023-04-04 10:33:08 -0700

    Added API URL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './scss/index.scss';
+import axios from 'axios'
 
 function App() {
 
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
